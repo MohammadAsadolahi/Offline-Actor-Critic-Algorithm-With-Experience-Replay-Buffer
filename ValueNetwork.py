6734119014255e295e82d87855b0ca80e41b7e3b
Mohammad Asadolahi <79268727+MohammadAsadolahi@users.noreply.github.com>
2024-01-11 00:00:22 -0800

    ValueNetwork class added
    The ValueNetwork file contains the ValueNetwork class which is a neural network used for value estimation during each state

---
+++ ValueNetwork.py
@@ -0,0 +1,12 @@
+import imports_file
+class ValueNetwork(nn.Module):
+    def __init__(self,inputShape,lr=0.0005):
+        super(ValueNet,self).__init__()
+        self.l1=nn.Linear(*inputShape,256)
+        self.l2=nn.Linear(256,256)
+        self.l3=nn.Linear(256,1)
+        self.optimizer=optim.Adam(self.parameters(),lr)
+    def forward(self,input):
+        output=F.relu(self.l1(input)) 
+        output=F.relu(self.l2(output))
+        return self.l3(output)
