5fa4d12d469b50286456636342aa279e27c7e640
MohammadAsadolahi <79268727+MohammadAsadolahi@users.noreply.github.com>
2024-01-14 00:24:29 -0800

    Agent python file added
    The python file  (.py) which contains reinforcement learning Agent class is added to the repository

---
+++ Agent.py
@@ -0,0 +1,12 @@
+class Agent():
+    def __init__(self, inputShape, outputShape, gamma=0.99, lr=5e-3):
+	pass
+
+    def save(self, state, action, reward, state_, done):
+	pass
+
+    def chooseAction(self, state):
+	pass
+
+    def learn(self, batchSize):
+	pass
